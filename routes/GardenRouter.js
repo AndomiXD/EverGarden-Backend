9e63a900a8410524d8dd9a2f5575267fcb2f3dea
shooqbinzaiman <shooqshowqi@gmail.com>
2025-10-24 10:24:31 +0300

    adding the GardenRouter

--- routes/GardenRouter.js
+++ routes/GardenRouter.js
@@ -1,1 +1,12 @@
+const router = require("express").Router()
+const middleware = require("../middleware")
+const controller = require("../controllers/GardenController")
+
+router.post(
+  "/create",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.createGarden
+)
+module.exports = router
 
