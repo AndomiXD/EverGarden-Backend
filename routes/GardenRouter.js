45f55cc588c4921f334a7d4f4d38cc0625f507eb
AndomiXD <vintin4some@gmail.com>
2025-10-26 02:15:32 +0300

    share post, see my and all posts

--- routes/GardenRouter.js
+++ routes/GardenRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const middleware = require("../middleware")
 const controller = require("../controllers/GardenController")
 
+//POST  create garden
 router.post(
   "/create",
   middleware.stripToken,
